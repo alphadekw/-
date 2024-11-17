f6c911569c88417df254149fa70d418c83904291
alphadekw <163714728+alphadekw@users.noreply.github.com>
2024-11-17 13:14:42 +0300

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,8 @@
+quantity = 12
+
+quantity_hour = 1.5
+
+name = 'Python'
+
+time_work = 0.125
+print('Курс:', name, 'Всего задач:', quantity, 'Затрачено времени:', quantity_hour, 'Среднее время выполнения', time_work)
